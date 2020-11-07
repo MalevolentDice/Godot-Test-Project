670b4884c1303dfc28a47bc77c16294dcc1247ab
MalevolentDice <MalevolentDice@users.noreply.github.com>
2020-11-07 23:09:24 +0100

    Moved player to bottom left and added velocity

--- Scenes/player.cs
+++ Scenes/player.cs
@@ -1,24 +1,30 @@
 using Godot;
 using System;
 
-public class player : Sprite {
-	public override void _Ready() {
-	  GD.Print("Test");
-	}
+public class player : Sprite
+{
+  private Vector2 _velocity = new Vector2(0, 0);
+  private readonly int _maxVelocity = 10;
+  private readonly int _acceleration = 5;
+  public override void _Ready()
+  {
+  }
+
+  public player()
+  {
+    // this.Position = new Vector2(32,288-32);
+  }
 
   // Called every frame. 'delta' is the elapsed time since the previous frame.
-  public override void _Process(float delta) {
-    if (Input.IsKeyPressed((int) KeyList.W)) {
-      this.Position += new Vector2(0, -1);
-    }
-    if (Input.IsKeyPressed((int) KeyList.S)) {
-      this.Position += new Vector2(0, 1);
-    }
-    if (Input.IsKeyPressed((int) KeyList.A)) {
-      this.Position += new Vector2(-1, 0);
-    }
-    if (Input.IsKeyPressed((int) KeyList.D)) {
-      this.Position += new Vector2(1, 0);
-    }
+  public override void _Process(float delta)
+  {
+    if (Input.IsKeyPressed((int)KeyList.A))
+      _velocity += new Vector2(-1, 0);
+    if (Input.IsKeyPressed((int)KeyList.D))
+      _velocity += new Vector2(1, 0);
+
+    _velocity.x = Math.Min(_velocity.x, _maxVelocity);
+    _velocity.x = Math.Max(_velocity.x, -_maxVelocity);
+    Position += _velocity * delta * _acceleration;
   }
 }
